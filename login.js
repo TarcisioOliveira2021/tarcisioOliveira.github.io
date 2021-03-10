25e412a5b74af6cbd447206f6bcc192af668d7b1
TarcisioOliveira2021 <79255361+TarcisioOliveira2021@users.noreply.github.com>
2021-03-10 13:54:30 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 function bomdia(){
-  alert('Bom dia sangue bom !');
-}  
+  alert("Bom dia sangue bom !");
+}
+
+function botaoEvento(){
+  var senha = document.querySelector("#inputPassword");
+  if(senha.lenght < 6){
+     alert( " A senha precisa ter 6 caracteres ");
+  } 
+  alert("Botão clicado");
+} 
+

7bd22cfc8388d04fba719c969414e255845d9268
TarcisioOliveira2021 <79255361+TarcisioOliveira2021@users.noreply.github.com>
2021-03-10 14:59:46 -0300

    Update login.js

--- login.js
+++ login.js
@@ -2,13 +2,13 @@ function salve(){
   alert('Um salve pra todo mundo !');
 }
 
-function botaoEvento(this){
+function botaoEvento(botao){
   var senha = document.querySelector("#inputPassword");
  
   if(senha.value.length < 6){
      alert(' A senha precisa ter 6 caracteres ');
   }
   alert('Botão clicado');
-  this.submit();
+  botao.submit();
 } 
 
